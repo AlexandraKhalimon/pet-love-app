cf96a47d9f67b2bab89fb51a5608b348714af7c0
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-02-10 22:52:12 +0200

    feat(home): add new homepage layout and styles with unified header and hero

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,11 +6,32 @@ import { useState } from "react";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
 import AuthNav from "../AuthNav/AuthNav";
 import Nav from "../Nav/Nav";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname();
+  const isHomepage = pathname === "/home";
 
-  return (
+  return isHomepage ? (
+    <header className={css.homeHeader}>
+      <div className={css.headerWrapper}>
+        <Link href="/home" className={css.logo}>
+          <svg width={76} height={20} className={css.icon}>
+            <use href="/icons.svg#icon-logo-2"></use>
+          </svg>
+        </Link>
+        <Nav className={css.nav} />
+        <AuthNav className={css.authNav} />
+        <button className={css.homeButton} onClick={() => setIsMenuOpen(true)}>
+          <svg width={32} height={32} className={css.icon}>
+            <use href="/icons.svg#icon-menu"></use>
+          </svg>
+        </button>
+        {isMenuOpen && <BurgerMenu onClose={() => setIsMenuOpen(false)} />}
+      </div>
+    </header>
+  ) : (
     <header className={css.header}>
       <Link href="/home" className={css.logo}>
         <svg width={76} height={20} className={css.icon}>
