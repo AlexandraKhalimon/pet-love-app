b1f4ab545a8d1361bb5c4c9c20ad42ec4b7f1175
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-02-11 22:55:31 +0200

    refactor(burger-menu): adjust layout and add new background-color variants

--- components/BurgerMenu/BurgerMenu.tsx
+++ components/BurgerMenu/BurgerMenu.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import { createPortal } from "react-dom";
 import css from "./BurgerMenu.module.css";
 import Nav from "../Nav/Nav";
 import AuthNav from "../AuthNav/AuthNav";
+import { usePathname } from "next/navigation";
 
 interface Props {
   onClose: () => void;
 }
 
 export default function BurgerMenu({ onClose }: Props) {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/home";
+
+  const menuBackground = isHomePage ? css.white : css.orange;
+  const icon = isHomePage ? css.blackIcon : css.whiteIcon;
+
   return createPortal(
     <div className={css.backdrop}>
-      <div className={css.menu}>
+      <div className={`${css.menu} ${menuBackground}`}>
         <button className={css.button} onClick={onClose}>
-          <svg width={32} height={32} className={css.icon}>
+          <svg width={32} height={32} className={icon}>
             <use href="/icons.svg#icon-cross-small"></use>
           </svg>
         </button>
