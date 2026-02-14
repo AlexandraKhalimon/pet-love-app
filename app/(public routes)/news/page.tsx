a66e8682f207d8afcdc3cb7454b65d7b758cdbfe
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-02-14 21:57:24 +0200

    feat(news): integrate Title and SearchField components and adjust layout

--- app/(public routes)/news/page.tsx
+++ app/(public routes)/news/page.tsx
@@ -1,14 +1,16 @@
 // Компонент закоментований для тестів
 
-// import SearchField from "@/components/SearchField/SearchField";
-// import css from "./page.module.css";
-// import Title from "@/components/Title/Title";
+import SearchField from "@/components/SearchField/SearchField";
+import css from "./page.module.css";
+import Title from "@/components/Title/Title";
 
-// export default function News() {
-//   return (
-//     <section className={css.section}>
-//       <Title title={"News"} />
-//       <SearchField />
-//     </section>
-//   );
-// }
+export default function News() {
+  return (
+    <section className={css.section}>
+      <div className={css.newsHeader}>
+        <Title title={"News"} />
+        <SearchField />
+      </div>
+    </section>
+  );
+}
