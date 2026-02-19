30d0bb9356e92ceffa20d1e6e73dd2cb628ef9ee
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-02-19 23:42:48 +0200

    feat(friends): split page into server and client components and add prefetch

--- app/(public routes)/friends/page.tsx
+++ app/(public routes)/friends/page.tsx
@@ -1,3 +1,21 @@
-export default function Friends() {
-  return <></>;
+import { fetchFriends } from "@/lib/api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import FriendsClient from "./Friends.client";
+
+export default async function FriendsPage() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["friends"],
+    queryFn: () => fetchFriends(),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <FriendsClient />
+    </HydrationBoundary>
+  );
 }
