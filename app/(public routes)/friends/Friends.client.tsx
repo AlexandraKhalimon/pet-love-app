6562b64e954fc6409fac668aaad6f7f8f572ff86
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-02-20 22:02:03 +0200

    feat(friends): implement FriendsClient data fetching

--- app/(public routes)/friends/Friends.client.tsx
+++ app/(public routes)/friends/Friends.client.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import FriendsList from "@/components/FriendsList/FriendsList";
+import { fetchFriends } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+
 export default function FriendsClient() {
-  return <></>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["friends"],
+    queryFn: fetchFriends,
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Sorry, there is an error</p>;
+  }
+
+  if (data) {
+    console.log(data);
+  }
+
+  return (
+    <section>
+      {data && data.length > 0 && <FriendsList friends={data} />}
+    </section>
+  );
 }
