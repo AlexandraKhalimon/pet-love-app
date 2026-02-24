f4eda7e13483e7913b695790543c617b5ea0336b
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-02-24 23:59:31 +0200

    test(notices): add temporary notices page for testing NoticesFilters

--- app/(public routes)/notices/page.tsx
+++ app/(public routes)/notices/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import NoticesFilters from "@/components/NoticesFilters/NoticesFilters";
+import {
+  fetchNoticeCategories,
+  fetchNoticeSex,
+  fetchNoticeSpecies,
+} from "@/lib/api";
+import { Category, Sex, Species } from "@/types/notice";
+import { useEffect, useState } from "react";
+
 export default function Notices() {
-  return <></>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [sexes, setSexes] = useState<Sex[]>([]);
+  const [types, setTypes] = useState<Species[]>([]);
+
+  useEffect(() => {
+    fetchNoticeCategories().then(setCategories);
+    fetchNoticeSex().then(setSexes);
+    fetchNoticeSpecies().then(setTypes);
+  }, [categories, sexes, types]);
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+  };
+  return (
+    <>
+      <NoticesFilters
+        search={handleSearch}
+        categories={categories}
+        sexes={sexes}
+        types={types}
+      />
+    </>
+  );
 }
