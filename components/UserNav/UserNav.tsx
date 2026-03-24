16bb3ad5a74ff4cdb021939a31f937959765ac63
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-03-24 23:27:41 +0200

    feat(user-nav): hide LogOutBtn component on mobile screens

--- components/UserNav/UserNav.tsx
+++ components/UserNav/UserNav.tsx
@@ -1,9 +1,10 @@
+import css from "./UserNav.module.css";
 import LogOutBtn from "../LogOutBtn/LogOutBtn";
 
 export default function UserNav() {
   return (
     <>
-      <LogOutBtn />
+      <LogOutBtn className={css.logout} />
     </>
   );
 }
