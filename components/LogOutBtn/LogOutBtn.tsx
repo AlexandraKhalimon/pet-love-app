637a237f973a24295e20df996ad1a00564716244
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-03-24 23:55:00 +0200

    feat(logout-btn): integrate ModalApproveAction into LogOutBtn and add type button

--- components/LogOutBtn/LogOutBtn.tsx
+++ components/LogOutBtn/LogOutBtn.tsx
@@ -1,9 +1,31 @@
+"use client";
+
 import css from "./LogOutBtn.module.css";
+import ModalApproveAction from "../ModalApproveAction/ModalApproveAction";
+import Modal from "../Modal/Modal";
+import { useState } from "react";
 
 interface Props {
   className?: string;
 }
 
 export default function LogOutBtn({ className }: Props) {
-  return <button className={`${css.button} ${className}`}>Log out</button>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        className={`${css.button} ${className}`}
+        onClick={() => setIsModalOpen(true)}
+        type="button"
+      >
+        Log out
+      </button>
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <ModalApproveAction />
+        </Modal>
+      )}
+    </>
+  );
 }
