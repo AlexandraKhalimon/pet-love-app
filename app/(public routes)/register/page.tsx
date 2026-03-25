50dfa5d0a7e6b6fbd6438d0d9b11a383f67273a2
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-03-25 23:35:12 +0200

    feat(register): implement PetBlock and Title components

--- app/(public routes)/register/page.tsx
+++ app/(public routes)/register/page.tsx
@@ -1,3 +1,11 @@
+import PetBlock from "@/components/PetBlock/PetBlock";
+import Title from "@/components/Title/Title";
+
 export default function Register() {
-  return <></>;
+  return (
+    <>
+      <PetBlock src="/images/pet-cat-mobile.jpg" alt="Cat" />
+      <Title title={"Registration"} />
+    </>
+  );
 }
