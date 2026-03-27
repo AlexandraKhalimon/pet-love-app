adf1e3b491a359a8e9f3764cde01db8027cc6f19
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-03-27 23:39:46 +0200

    feat(pet-block): replace next/image with img and add art direction

--- components/PetBlock/PetBlock.tsx
+++ components/PetBlock/PetBlock.tsx
@@ -2,14 +2,26 @@ import Image from "next/image";
 
 interface Props {
   src: string;
+  tablet: string;
+  desktop: string;
   alt: string;
   className?: string;
 }
 
-export default function PetBlock({ src, alt, className }: Props) {
+export default function PetBlock({
+  src,
+  tablet,
+  desktop,
+  alt,
+  className,
+}: Props) {
   return (
     <div className={className}>
-      <Image src={src} alt={alt} width={335} height={280} priority />
+      <picture>
+        <source media="(min-width: 1280px)" srcSet={desktop} />
+        <source media="(min-width: 768px)" srcSet={tablet} />
+        <img src={src} alt={alt} loading="eager" />
+      </picture>
     </div>
   );
 }
