b4482cef0f75cf8ecd31b01b9175bc2b98f48638
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-03-30 22:21:43 +0300

    feat(login): add PetBlock component with responsive images

--- app/(public routes)/login/page.tsx
+++ app/(public routes)/login/page.tsx
@@ -1,3 +1,15 @@
+import css from "./page.module.css";
+import PetBlock from "@/components/PetBlock/PetBlock";
+
 export default function Login() {
-  return <></>;
+  return (
+    <section className={css.section}>
+      <PetBlock
+        src="/images/pet-corgi-mobile.png"
+        tablet="/images/pet-corgi-tablet.png"
+        desktop="/images/pet-corgi-desktop.png"
+        alt="Corgi"
+      />
+    </section>
+  );
 }
