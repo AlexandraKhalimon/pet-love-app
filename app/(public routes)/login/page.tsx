8d1e19a0da548c4515b40d23ee1041acf092cb14
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-03-30 22:47:07 +0300

    feat(login): integrate Title and LoginForm into login page

--- app/(public routes)/login/page.tsx
+++ app/(public routes)/login/page.tsx
@@ -1,5 +1,7 @@
 import css from "./page.module.css";
 import PetBlock from "@/components/PetBlock/PetBlock";
+import Title from "@/components/Title/Title";
+import LoginForm from "@/components/LoginForm/LoginForm";
 
 export default function Login() {
   return (
@@ -10,6 +12,10 @@ export default function Login() {
         desktop="/images/pet-corgi-desktop.png"
         alt="Corgi"
       />
+      <div className={css.form}>
+        <Title title="Log in" />
+        <LoginForm />
+      </div>
     </section>
   );
 }
