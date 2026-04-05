ff86fe20a58464816503dd4a31c8d355c584789e
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-04-05 22:45:16 +0300

    feat(profile): add user data fetching

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
 import css from "./page.module.css";
 import UserCard from "@/components/UserCard/UserCard";
+import { fetchUserFullInfo } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ProfilePage() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["user"],
+    queryFn: fetchUserFullInfo,
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Sorry, there is an error</p>;
+  }
+
+  console.log(data);
+
   return (
     <section className={css.section}>
-      <UserCard />
+      {data && <UserCard user={data} />}
     </section>
   );
 }
