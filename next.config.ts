834b9d92d0a0fbe1c97f79878d75995a83c75e11
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-04-07 23:40:34 +0300

    fix(config): allow images from new host

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,8 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "media4.giphy.com" },
       { protocol: "https", hostname: "ftp.goit.study" },
       { protocol: "https", hostname: "test.png" },
+      { protocol: "https", hostname: "test2.webp" },
+      { protocol: "https", hostname: "test.webp" },
     ],
   },
   reactCompiler: true,
