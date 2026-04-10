38c2e21abc87530c1a4bdfec20e8d6dc43333551
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-04-10 23:37:47 +0300

    feat(modal-edit-user): create ModalEditUser with form layout

---
+++ components/ModalEditUser/ModalEditUser.tsx
@@ -0,0 +1,88 @@
+import css from "./ModalEditUser.module.css";
+import { FullUser } from "@/types/user";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+
+interface Props {
+  user: FullUser;
+}
+
+interface EditUserValues {
+  avatar: string;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export default function ModalEditUser({ user }: Props) {
+  const { register, handleSubmit } = useForm<EditUserValues>({
+    defaultValues: {
+      avatar: user.avatar || "",
+      name: user.name || "",
+      email: user.email || "",
+      phone: user.phone || "",
+    },
+  });
+
+  const onSubmit = (data: EditUserValues) => console.log(data);
+
+  return (
+    <div className={css.modal}>
+      <h2 className={css.heading}>Edit information</h2>
+      {user.avatar ? (
+        <Image
+          src={user.avatar}
+          alt="Profile photo"
+          width={80}
+          height={80}
+          className={css.avatar}
+        />
+      ) : (
+        <div className={css.image}>
+          <svg className={css.icon} width={36} height={36}>
+            <use href="/icons.svg#icon-user"></use>
+          </svg>
+        </div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={css.formInputs}>
+          <label className={css.label}>
+            <input
+              {...register("avatar")}
+              type="text"
+              className={css.input}
+              placeholder="Photo"
+            />
+          </label>
+          <label className={css.label}>
+            <input
+              {...register("name")}
+              type="text"
+              className={css.input}
+              placeholder="Name"
+            />
+          </label>
+          <label className={css.label}>
+            <input
+              {...register("email")}
+              type="email"
+              className={css.input}
+              placeholder="Email"
+            />
+          </label>
+          <label className={css.label}>
+            <input
+              {...register("phone")}
+              type="tel"
+              className={css.input}
+              placeholder="Phone number"
+            />
+          </label>
+        </div>
+        <button type="submit" className={css.submit}>
+          Go to profile
+        </button>
+      </form>
+    </div>
+  );
+}
