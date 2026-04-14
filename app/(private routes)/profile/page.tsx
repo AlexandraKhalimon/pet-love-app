e15427d005426bcda27c1b01db2c898ac6365d93
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-04-14 21:39:07 +0300

    feat(profile-page): display favorite and viewed notices by tabs

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -4,6 +4,7 @@ import css from "./page.module.css";
 import UserCard from "@/components/UserCard/UserCard";
 import { fetchUserFullInfo } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
+import MyNotices from "@/components/MyNotices/MyNotices";
 
 export default function ProfilePage() {
   const { data, isLoading, error } = useQuery({
@@ -24,6 +25,12 @@ export default function ProfilePage() {
   return (
     <section className={css.section}>
       {data && <UserCard user={data} pets={data.pets} />}
+      {data && (
+        <MyNotices
+          favoriteNotices={data.noticesFavorites}
+          viewedNotices={data.noticesViewed}
+        />
+      )}
     </section>
   );
 }
