9966a68df81b98e2c5137244a6baa602b0a32b47
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-04-21 23:48:36 +0300

    feat(user-types): add auth types and make notices and pets fields optional

--- types/user.ts
+++ types/user.ts
@@ -5,14 +5,14 @@ export type User = {
   name: string;
   email: string;
   token: string;
-  noticesFavorites: Notice[];
+  noticesFavorites?: Notice[];
 };
 
 export type FullUser = User & {
   avatar?: string;
   phone?: string;
-  noticesViewed: Notice[];
-  pets: Pet[];
+  noticesViewed?: Notice[];
+  pets?: Pet[];
   createdAt: string;
   updatedAt?: string;
 };
@@ -27,6 +27,17 @@ export type Pet = {
   sex: Sex;
   createdAt: string;
   updatedAt?: string;
+};
+
+export type RegisterUser = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type LoginUser = {
+  email: string;
+  password: string;
 };
 
 export type UserEdit = {
