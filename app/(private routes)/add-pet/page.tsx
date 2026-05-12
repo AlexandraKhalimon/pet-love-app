0d12c3d65b04613c961bc2691adfd47a2a06d082
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-05-12 23:30:04 +0300

    feat(add-pet-page): integrate PetBlock component into add-pet page

--- app/(private routes)/add-pet/page.tsx
+++ app/(private routes)/add-pet/page.tsx
@@ -1,5 +1,16 @@
 import css from "./page.module.css";
+import PetBlock from "@/components/PetBlock/PetBlock";
 
 export default function AddPetPage() {
-  return <section className={css.section}></section>;
+  return (
+    <section className={css.section}>
+      <PetBlock
+        src="/images/pet-dog-mobile.png"
+        tablet="/images/pet-dog-tablet.png"
+        desktop="/images/pet-dog-desktop.png"
+        alt="Dog with glasses"
+        className={css.image}
+      />
+    </section>
+  );
 }
