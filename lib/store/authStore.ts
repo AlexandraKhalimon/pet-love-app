0cf863bd77bd7cca8b6654513717d00889a78bbc
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-06-16 23:18:21 +0300

    feat(store): create zustand auth store with persist

---
+++ lib/store/authStore.ts
@@ -0,0 +1,25 @@
+import { User } from "@/types/user";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface AuthStore {
+  user: User | null;
+  setUser: (user: User) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      logout: () => set({ user: null }),
+    }),
+    {
+      name: "auth",
+      partialize: (state) => ({
+        user: state.user,
+      }),
+    },
+  ),
+);
