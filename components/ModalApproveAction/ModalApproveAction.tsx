cfca2e60acea87b66090a5f4336dec3dc95e1763
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-06-16 23:46:11 +0300

    feat(modal-approve): connect logout button with zustand auth store

--- components/ModalApproveAction/ModalApproveAction.tsx
+++ components/ModalApproveAction/ModalApproveAction.tsx
@@ -1,16 +1,39 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
 import css from "./ModalApproveAction.module.css";
+import { logoutUser } from "@/lib/api";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/lib/store/authStore";
 
 interface Props {
   onClose: () => void;
 }
 
 export default function ModalApproveAction({ onClose }: Props) {
+  const logout = useAuthStore((state) => state.logout);
+  const router = useRouter();
+
+  const { mutate } = useMutation({
+    mutationFn: logoutUser,
+    onSuccess: () => {
+      console.log("User logged out");
+      onClose();
+      logout();
+      router.push("/home");
+    },
+  });
+
+  const handleClick = () => mutate();
+
   return (
     <div className={css.modal}>
       <div className={css.image}>🐈</div>
       <p className={css.question}>Already leaving?</p>
       <div className={css.approval}>
-        <button className={css.yes}>Yes</button>
+        <button className={css.yes} onClick={handleClick}>
+          Yes
+        </button>
         <button className={css.cancel} onClick={onClose}>
           Cancel
         </button>
