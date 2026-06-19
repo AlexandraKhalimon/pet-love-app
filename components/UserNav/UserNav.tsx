f15ad8b9690ed647f2a9183a5cb55b99fcde5c60
Alexandra Khalimon <alexandrakhalimon25@gmail.com>
2026-06-19 23:59:50 +0300

    style(user-nav): add dynamic color change for logout button

--- components/UserNav/UserNav.tsx
+++ components/UserNav/UserNav.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import css from "./UserNav.module.css";
 import LogOutBtn from "../LogOutBtn/LogOutBtn";
+import { usePathname } from "next/navigation";
 
 export default function UserNav() {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/home";
+
+  const logout = isHomePage ? css.homeLogout : css.logout;
+
   return (
     <>
-      <LogOutBtn className={css.logout} />
+      <LogOutBtn className={logout} />
     </>
   );
 }
